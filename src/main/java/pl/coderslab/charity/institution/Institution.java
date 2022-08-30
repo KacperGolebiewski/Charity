15f628ed633ecd989fddf8d0bafe3ac0947bea8d
golebiewskii.kacper@gmail.com <golebiewskii.kacper@gmail.com>
2022-08-30 13:46:46 +0200

    Institution entity/repo added

---
+++ src/main/java/pl/coderslab/charity/institution/Institution.java
@@ -0,0 +1,29 @@
+package pl.coderslab.charity.institution;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Institution {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+}
